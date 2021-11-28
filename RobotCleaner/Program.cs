3e3125cf3a2dd9e86cd8bd5c68eb083fcf45fbb1
Alvaro Fernandez <alvarofetero@gmail.com>
2021-11-28 18:13:18 +0100

    refactor input process

--- RobotCleaner/Program.cs
+++ RobotCleaner/Program.cs
@@ -8,47 +8,14 @@ namespace RobotCleanerClient
     {
         static void Main(string[] args)
         {
-                var numberOfCommands = GetNumberOfCommandsFromInput();
+            var inputReader = new InputReader();
+            inputReader.ReadInput();
+            
+            var robotCleaner = new RobotCleaner(inputReader.InitialPosition);
+            var placesCleaned = robotCleaner.Clean(inputReader.Commands.ToArray());
 
-                var initialPosition = ReadPositionFromInput();
-                var commands = GetCommandsFromInput(numberOfCommands);
-                var robotCleaner = new RobotCleaner(initialPosition);
-                var placesCleaned = robotCleaner.Clean(commands.ToArray());
-
-                Console.WriteLine($" => Cleaned :{placesCleaned}");
+            Console.WriteLine($" => Cleaned :{placesCleaned}");
         }
-
-        private static Position ReadPositionFromInput()
-        {
-            Console.Write("Enter initial position >");
-            var secondLine = Console.ReadLine();
-            var secondLineArray = secondLine.Split(' ');
-            var startingX = int.Parse(secondLineArray[0]);
-            var startingY = int.Parse(secondLineArray[1]);
-            return new Position(startingX, startingY);
-        }
-
-        private static List<string> GetCommandsFromInput(int numberOfCommads)
-        {
-            var commands = new List<string>();
-            string oneCommand;
-            for (int i = 0; i < numberOfCommads; i++)
-            {
-                Console.Write("Enter commands >");
-                oneCommand = Console.ReadLine();
-                commands.Add(oneCommand);
-            }
-            return commands;
-        }
-
-        private static int GetNumberOfCommandsFromInput()
-        {
-            Console.Write("Enter number of commands >");
-            var firstLine = Console.ReadLine();
-            return int.Parse(firstLine);
-        }
-
-
 
     }
 }
