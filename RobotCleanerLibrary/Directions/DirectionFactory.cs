944aab8990ee5adba5841003ff4fcdf524b1043e
Alvaro Fernandez <alvarofetero@gmail.com>
2021-12-11 14:43:58 +0100

    remove unreacheable code because of return

--- RobotCleanerLibrary/Directions/DirectionFactory.cs
+++ RobotCleanerLibrary/Directions/DirectionFactory.cs
@@ -10,16 +10,12 @@ namespace RobotCleanerLibrary
             {
                 case "E":
                     return new East();
-                    break;
                 case "W":
                     return new West();
-                    break;
                 case "N":
                     return new North();
-                    break;
                 case "S":
                     return new South();
-                    break;
                 default:
                     throw new NotSupportedException($"Direction {direction} is not supported.");
             }
